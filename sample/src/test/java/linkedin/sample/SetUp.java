77a385d74c4fd9d1ca70e7283d240e7a847971da
saidsure <98847344+saidsure@users.noreply.github.com>
2022-02-02 13:19:39 -0800

    Adding listener to setup class

--- sample/src/test/java/linkedin/sample/SetUp.java
+++ sample/src/test/java/linkedin/sample/SetUp.java
@@ -1,9 +1,17 @@
 package linkedin.sample;
 
+import java.io.File;
+import java.time.LocalDateTime;
+
+import org.apache.commons.io.FileUtils;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
  
-public class SetUp {
+public class SetUp implements ITestListener {
  
    public static WebDriver driver;
    public static void lauchBrowser(){
@@ -29,4 +37,55 @@ public class SetUp {
 	   }
 	   
    }
+   
+   public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+	 TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+	 File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+	 File DestFile=new File(fileWithPath);
+	 FileUtils.copyFile(SrcFile, DestFile);
+	 }
+   
+   public void onTestFailedButWithinSuccessPercentage(ITestResult result) {   }
+
+@Override
+public void onTestStart(ITestResult result) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void onTestSuccess(ITestResult result) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void onTestFailure(ITestResult result) {
+	//on test failure take screenshot
+	try {
+		takeSnapShot(driver,"/Users/skota/Downloads/Screenshot/Screenshot" + LocalDateTime.now()+".png");
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
+
+@Override
+public void onTestSkipped(ITestResult result) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void onStart(ITestContext context) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void onFinish(ITestContext context) {
+	// TODO Auto-generated method stub
+	
+}
+
 }
